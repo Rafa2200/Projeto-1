9de787f953eaf4782b62f2def953e06bb6280415
Rafa2200 <rafaela22042008@gmail.com>
2024-08-29 18:10:47 -0300

    SALVANDO

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(`rgba(247,151,168,0.85)`);
+    stroke(`black`)
+    fill(`rgb(0,0,7)`)
+    rect(mouseX, mouseY, 50, 100 )
+  }
